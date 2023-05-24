63389e74fd9e31aaa7166fadf3c5e278b577d781
Gomez Rogelio (Nokia) <gomez.rogelio@nokia.com>
2023-05-24 11:48:51 -0600

    Adding package for charts

--- lib/widgets/charts/chart_bar.dart
+++ lib/widgets/charts/chart_bar.dart
@@ -1,4 +1,5 @@
 import 'package:d_chart/d_chart.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
@@ -14,32 +15,86 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 400,
       child: Column(
         children: [
+          // SizedBox(
+          //   height: 200,
+          //   child: DChartBar(
+          //     data: [
+          //       {
+          //         'id': 'Bar',
+          //         'data': [
+          //           {'domain': 'Food', 'measure': 156.0},
+          //           {'domain': 'Leaisure', 'measure': 134.5},
+          //           {'domain': 'Travel', 'measure': 45.6},
+          //           {'domain': 'gifts', 'measure': 56.7},
+          //         ],
+          //       },
+          //     ],
+          //     domainLabelPaddingToAxisLine: 16,
+          //     axisLineTick: 2,
+          //     axisLinePointTick: 2,
+          //     axisLinePointWidth: 10,
+          //     axisLineColor: Colors.green,
+          //     measureLabelPaddingToAxisLine: 16,
+          //     barColor: (barData, index, id) => Colors.green,
+          //     showBarValue: true,
+          //   ),
+          // ),
+
           SizedBox(
-            height: 100,
-            child: DChartBar(
-              data: const [
-                {
-                  'id': 'Bar',
-                  'data': [
-                    {'Name': 10},
-                    {'Another name': 40.0},
-                    {'10': 10},
-                    {'50': 50}
+            height: 200,
+            child: BarChart(
+              BarChartData(
+                  gridData: FlGridData(
+                    show: true,
+                    checkToShowVerticalLine: (value) => value % 5 == 0,
+                    getDrawingVerticalLine: (value) => FlLine(
+                      color: Colors.grey,
+                      strokeWidth: 0.5,
+                    ),
+                  ),
+                  barGroups: [
+                    BarChartGroupData(x: 0, barRods: [
+                      BarChartRodData(
+                        toY: 8,
+                        color: Colors.blue,
+                      )
+                    ]),
+                    BarChartGroupData(x: 1, barRods: [
+                      BarChartRodData(
+                        toY: 12,
+                        color: Colors.blue,
+                      )
+                    ]),
+                    BarChartGroupData(x: 2, barRods: [
+                      BarChartRodData(
+                        toY: 14,
+                        color: Colors.blue,
+                      )
+                    ]),
+                    BarChartGroupData(x: 3, barRods: [
+                      BarChartRodData(
+                        toY: 13,
+                        color: Colors.green,
+                      )
+                    ])
                   ],
-                },
-              ],
-              domainLabelPaddingToAxisLine: 16,
-              axisLineTick: 2,
-              axisLinePointWidth: 10,
-              axisLineColor: Colors.black,
-              measureLabelPaddingToAxisLine: 16,
-              barColor: (barData, index, id) => Colors.black,
-              showBarValue: true,
+                  alignment: BarChartAlignment.spaceAround,
+                  maxY: 20,
+                  barTouchData: BarTouchData(enabled: false),
+                  titlesData: FlTitlesData(
+                    show: true,
+                    bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(),
+                    ),
+                  )),
+              swapAnimationDuration: const Duration(milliseconds: 150),
+              swapAnimationCurve: Curves.linear,
             ),
           ),
+
           SizedBox(
             height: 200,
             child: DChartPie(
